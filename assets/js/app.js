7ae81cdeb3450bb0f3ec23bda6e4c4431cfaefdf
victor dias <victor.dias@meumobi.com>
2018-06-07 10:51:41 +0100

    feat: Automatically generate anchor tags for all headings (h2, h3, etc. h1 is reserved for article header)

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,18 @@
+
 $( document ).ready(function() {
+	// prevent line-breaks in links and make open in new tab
+	$('div.article_body a').not('[rel="footnote"], [rev="footnote"]').html(function(i, str) {
+	    return str.replace(/ /g,'&nbsp;');
+	}).attr('target','_blank');
+
+	// Give article headings direct links to anchors
+	$('article h2, article h3, article h4, article h5, article h6').filter('[id]').each(function () {
+	$(this).append('<a href="#' + $(this).attr("id") + '" target="_top"><i class="fa fa-link" style="display:none"></i></a>');
+	});
+
+	$('article h2, article h3, article h4, article h5, article h6').filter('[id]').hover(function () {
+	$(this).find("i").toggle();
+	});
 
 	/* Sidebar height set */
 	$('.sidebar').css('min-height',$(document).height());
@@ -12,10 +26,4 @@ $( document ).ready(function() {
 	contact_list.mouseenter(function(){ scontacts.fadeIn(); });
 	
 	contact_list.mouseleave(function(){ scontacts.fadeOut(); });
-
-	// prevent line-breaks in links and make open in new tab
-	$('div.article_body a').not('[rel="footnote"], [rev="footnote"]').html(function(i, str) {
-	    return str.replace(/ /g,'&nbsp;');
-	}).attr('target','_blank');
-
 });
